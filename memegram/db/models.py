16f6a9bde6027f068b4994334ac618a6fdd61b27
RajaRaviVarma <rajaravivarma.ar@gmail.com>
2024-01-09 20:04:34 +0530

    Setup a has many through association for images and tags

--- memegram/db/models.py
+++ memegram/db/models.py
@@ -1,4 +1,6 @@
-from peewee import Model, SqliteDatabase, CharField
+from contextlib import contextmanager
+
+from peewee import Model, DeferredThroughModel, SqliteDatabase, CharField, ForeignKeyField, ManyToManyField
 
 from memegram.db import DB_FILE_PATH
 
@@ -9,8 +11,18 @@ class BaseModel(Model):
     class Meta:
         database = db
 
+    @classmethod
+    @contextmanager
+    def transaction(cls):
+        with cls._meta.database.atomic():
+            yield
+
+ImageTagThroughDeferred = DeferredThroughModel()
 
 class Image(BaseModel):
+    class Meta:
+        table_name = "images"
+
     filename = CharField()
     filepath = CharField()
     digest = CharField(unique=True)
@@ -21,5 +33,22 @@ class Image(BaseModel):
             f"filepath: {self.filepath}, digest: {self.digest}>"
         )
 
+class Tag(BaseModel):
+    class Meta:
+        table_name = "tags"
+
+    name = CharField(unique=True)
+    images = ManyToManyField(Image, through_model=ImageTagThroughDeferred, backref="tags")
+
+class ImageTag(BaseModel):
+    class Meta:
+        table_name = 'image_tags'
+
+    tags = ForeignKeyField(Tag, backref="image_tags")
+    images = ForeignKeyField(Image, backref="image_tags")
+
+ImageTagThroughDeferred.set_model(ImageTag)
 
 Image.create_table()
+Tag.create_table()
+ImageTag.create_table()
